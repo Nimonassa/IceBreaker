41e746977a896073388ce3ae3a146259414435ce
DoctorFinger <144325868+DoctorFinger@users.noreply.github.com>
2026-04-06 09:05:29 +0300

    spawn point

--- Assets/_Game/_Scripts/Game/SpawnPoint.cs
+++ Assets/_Game/_Scripts/Game/SpawnPoint.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class SpawnPoint : MonoBehaviour
 {
+    private void Reset()
+    {
+        this.gameObject.name = "Spawn Point";
+#if UNITY_EDITOR
+        Texture2D icon = EditorGUIUtility.IconContent("sv_label_6").image as Texture2D;
+        EditorGUIUtility.SetIconForObject(this.gameObject, icon);
+#endif
+    }
+    
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.cyan;
